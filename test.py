c60d76424dbc726d035dd95d96323e5fba745bdc
Darival <darivalrcv@gmail.com>
2018-02-02 18:41:10 -0600

    complete dearing values

--- test.py
+++ test.py
@@ -10,20 +10,28 @@ import pyocr.builders
 TOOL = pyocr.get_available_tools()[0]
 LANG = TOOL.get_available_languages()[2]
 
-req_image = []
-final_text = []
-
 IMAGE_PDF = Image(filename="./pdf/test.pdf")
 image_jpeg = IMAGE_PDF.convert('jpeg')
 
 for img in image_jpeg.sequence:
     img_page = Image(image=img)
     img_page.type = 'grayscale'
-    with img[304:958, 410:476] as cropped:
-        image = cropped.make_blob('jpeg')
-        name = TOOL.image_to_string(
-            PI.open(io.BytesIO(image)),
-            lang=LANG,
-            builder=pyocr.builders.TextBuilder()
-        )
-        print(name)
+    profile = {
+        "name": [302, 968, 410, 476],
+        "test_type": [394, 1492, 726, 790],
+        "keys": [186, 566, 846, 1684],
+        "values": [876, 1168, 846, 1684],
+    }
+    #img_page.save(filename='pikachu.jpg')
+    
+    for k, v in profile.items():
+        with img_page[v[0]:v[1], v[2]:v[3]] as cropped:
+            image = cropped.make_blob('jpeg')
+            name = TOOL.image_to_string(
+                PI.open(io.BytesIO(image)),
+                lang=LANG,
+                builder=pyocr.builders.TextBuilder()
+            )
+            # TODO this should be on a function
+            text = list(filter(None, name.split('\n')))
+            print(k, text, sep=' = ')
